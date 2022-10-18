d2ca442fe7eacfad2808223ac773fb2e779a2a30
CStorn1001 <christian.stornelli@students.mq.edu.au>
2022-10-18 14:28:03 +1100

    updated model performance

---
+++ hourlydata_method.py
@@ -0,0 +1,64 @@
+#data manipulation libraries
+import pandas as pd
+import numpy as np 
+
+#data visualisation libraries
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+# import matplotlib as plt
+import seaborn as sns
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+from pandas.plotting import autocorrelation_plot
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+from matplotlib import pyplot
+
+#stat models libraries 
+import statsmodels.api as sm
+from statsmodels.tsa.stattools import adfuller#for augmented Dickey-Fuller test
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.seasonal import seasonal_decompose
+import statsmodels.tsa.api as smt #autocovariance plot
+
+#sklearn predictive modelling
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import accuracy_score
+
+#technical analysis libaries
+import talib as ta # Used to perform Technical analysis of financial market data
+
+#others libraries 
+import math
+import datetime
+
+#libaries to collect path for data extraction
+import os
+import glob
+
+directory = os.getcwd()
+filepath = f"{directory}\Hourly_data"
+dfaudusd_hr = pd.read_csv(f"{filepath}\dfaudusd_hr.csv")
+dfeuraud_hr = pd.read_csv(f"{filepath}\dfeuraud_hr.csv")
+dfeurgbp_hr = pd.read_csv(f"{filepath}\dfeurgbp_hr.csv")
+dfeurusd_hr = pd.read_csv(f"{filepath}\dfeurusd_hr.csv")
+dfgbpusd_hr = pd.read_csv(f"{filepath}\dfgbpusd_hr.csv")
+dfaudusd_hr.head()
+
+dfaudusd_hr = dfaudusd_hr.drop(dfaudusd_hr.columns[[0]], axis=1)
+dfeuraud_hr = dfeuraud_hr.drop(dfeuraud_hr.columns[[0]], axis=1)
+dfeurgbp_hr = dfeurgbp_hr.drop(dfeurgbp_hr.columns[[0]], axis=1)
+dfeurusd_hr = dfeurusd_hr.drop(dfeurusd_hr.columns[[0]], axis=1)
+dfgbpusd_hr = dfgbpusd_hr.drop(dfgbpusd_hr.columns[[0]], axis=1)
+dfaudusd_hr.head()
+
+dfs= [dfaudusd_hr, dfeuraud_hr, dfeurgbp_hr, dfeurusd_hr, dfgbpusd_hr]
+for i in range(len(dfs)):
+    dfs[i][['Date', 'Time']] = dfs[i]['datetime'].str.split(' ', expand=True)
+    dfs[i][['Time', 'Useless']]  = dfs[i]['Time'].str.split('+', expand=True)
+dfaudusd_hr.head()
